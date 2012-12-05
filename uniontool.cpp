5cc8da736effe2d4f630683e2428fbe6c1e19a68
Ondřej Navrátil <nav.ondrej@gmail.com>
2012-12-05 21:29:13 +0100

    Nachysta uniontool na implementaci

--- uniontool.cpp
+++ uniontool.cpp
@@ -5,15 +5,73 @@ UnionTool::UnionTool(DisjointSetsScene *scene, QObject *parent) : Tool(scene, pa
 
 void UnionTool::selectTool()
 {
-
+    firstNode = NULL;
 }
 
 void UnionTool::deselectTool()
 {
-
+    if(firstNode != NULL)
+        scene->unhighlightNode(firstNode);
 }
 
 void UnionTool::nodeClicked(Node *node)
 {
+    if(firstNode == NULL)
+    {
+        firstNode = node;
+        scene->highlightNode(node);
+    }
+    else if(firstNode == node)
+    {
+        scene->unhighlightNode(firstNode);
+        firstNode = NULL;
+    }
+    else
+    {
+        simulate(this->firstNode, node);
+        firstNode = NULL;
+        scene->unhighlightNode(firstNode);
+    }
+}
 
+void UnionTool::simulate(Node *n1, Node *n2)
+{
+    Simulation *simulation = new Simulation();
+    DisjointSetsScene *s =  new DisjointSetsScene(NULL, scene->forest, scene->representation);
+    s->resetScene();
+    s->highlightNode(n1);
+    s->highlightNode(n2);
+    while(n1->parent != NULL)
+        n1 = n1->parent;
+    while(n2->parent != NULL)
+        n2 = n2->parent;
+    simulation->scenes.append(s);
+
+    s =  new DisjointSetsScene(NULL, scene->forest, scene->representation);
+    s->resetScene();
+    s->highlightNode(n1);
+    s->highlightNode(n2);
+    simulation->scenes.append(s);
+    if(n1 != n2)
+    {
+        if(optimize)
+        {
+            if(scene->representation == DisjointSetsScene::TREE)
+            {
+                //check heights and possibly swap n1, n2 so that n1 is shorter then n2
+            }
+            else
+            {
+                //check lengths and possibly swap n1, n2 so that n1 is shorter then n2
+            }
+
+        }
+        if(scene->representation == DisjointSetsScene::TREE)
+            simulateTree(s, n1, n2);
+        else
+            simulateList(s, n1, n2);
+
+    }
+    simulation->currentScene = simulation->scenes.begin();
+    emit signalSimulate(simulation);
 }
--- uniontool.h
+++ uniontool.h
@@ -8,10 +8,15 @@ class UnionTool: public Tool
 public:
     UnionTool(DisjointSetsScene *scene, QObject *parent = NULL);
     bool optimize;
+    Node *firstNode;
 
     void nodeClicked(Node *node);
     void selectTool();
     void deselectTool();
+    void simulate(Node *n1, Node *n2);
+
+    void simulateTree(Simulaion *s, Node *n1, Node *n2);
+    void simulateList(Simulaion *s, Node *n1, Node *n2);
 };
 
 #endif // UNIONTOOL_H
